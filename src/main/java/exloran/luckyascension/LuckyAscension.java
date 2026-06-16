c1c237f1959d493836e8a6d143b02c113a3f1c85
Yukile394 <muhamedmuhet@gmail.com>
2026-06-17 02:21:01 +0300

    Refactor package structure for LuckyAscension

--- src/main/java/exloran/luckyascension/LuckyAscension.java
+++ src/main/java/exloran/luckyascension/LuckyAscension.java
@@ -1,19 +1,13 @@
-package com.luckyascension;
+package exloran.luckyascension;
 
-import com.luckyascension.event.MobDeathHandler;
-import com.luckyascension.event.PlayerJoinHandler;
-import com.luckyascension.item.LuckBoostItem;
-import com.luckyascension.item.LuckEggItem;
+import exloran.luckyascension.event.MobDeathHandler;
+import exloran.luckyascension.event.PlayerJoinHandler;
+import exloran.luckyascension.item.LuckBoostItem;
+import exloran.luckyascension.item.LuckEggItem;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
-import net.fabricmc.fabric.api.event.player.UseItemCallback;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,7 +17,6 @@ public class LuckyAscension implements ModInitializer {
     public static final String MOD_ID = "luckyascension";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    // Item tanımları
     public static Item LUCK_BOOST;
     public static Item LUCK_EGG;
 
@@ -31,7 +24,6 @@ public class LuckyAscension implements ModInitializer {
     public void onInitialize() {
         LOGGER.info("[LuckyAscension] Mod baslatiliyor...");
 
-        // Item'ları kaydet
         LUCK_BOOST = Registry.register(
             Registries.ITEM,
             Identifier.of(MOD_ID, "luck_boost"),
@@ -44,10 +36,9 @@ public class LuckyAscension implements ModInitializer {
             new LuckEggItem(new Item.Settings().maxCount(16))
         );
 
-        // Event handler'ları kaydet
         PlayerJoinHandler.register();
         MobDeathHandler.register();
 
-        LOGGER.info("[LuckyAscension] Mod yuklendi! Sans yolculugun baslasın!");
+        LOGGER.info("[LuckyAscension] Mod yuklendi!");
     }
 }
