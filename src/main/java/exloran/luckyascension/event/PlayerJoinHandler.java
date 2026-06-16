69a7d49b9d0f2189be97880e7f2b07fcdccbf567
Yukile394 <muhamedmuhet@gmail.com>
2026-06-17 02:35:49 +0300

    Refactor guide book creation in PlayerJoinHandler

--- src/main/java/exloran/luckyascension/event/PlayerJoinHandler.java
+++ src/main/java/exloran/luckyascension/event/PlayerJoinHandler.java
@@ -4,14 +4,12 @@ import exloran.luckyascension.util.LuckData;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.WrittenBookContentComponent;
-import net.minecraft.component.type.WritableBookContentComponent;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.text.RawFilteredPair;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class PlayerJoinHandler {
@@ -49,73 +47,26 @@ public class PlayerJoinHandler {
     private static ItemStack createGuideBook() {
         ItemStack book = new ItemStack(Items.WRITTEN_BOOK);
 
-        List<RawFilteredPair<Text>> pages = new ArrayList<>();
-
-        pages.add(RawFilteredPair.of(Text.Serialization.fromJsonString(
-            "{\"text\":\"\",\"extra\":[" +
-            "{\"text\":\"Lucky Ascension\\n\",\"color\":\"gold\",\"bold\":true}," +
-            "{\"text\":\"\\nHos Geldin!\\n\",\"color\":\"yellow\"}," +
-            "{\"text\":\"\\nDusman yaratiklar oldurunce OP esyalar kazan ve sans seviyeni yukseltin!\",\"color\":\"gray\"}" +
-            "]}", null)));
-
-        pages.add(RawFilteredPair.of(Text.Serialization.fromJsonString(
-            "{\"text\":\"\",\"extra\":[" +
-            "{\"text\":\"Sans Sistemi\\n\",\"color\":\"gold\",\"bold\":true}," +
-            "{\"text\":\"\\nSeviye 1-5:\\n\",\"color\":\"gray\"}," +
-            "{\"text\":\"  x1 odul\\n\",\"color\":\"green\"}," +
-            "{\"text\":\"Seviye 6-10:\\n\",\"color\":\"gray\"}," +
-            "{\"text\":\"  x2 odul\\n\",\"color\":\"green\"}," +
-            "{\"text\":\"Seviye 11-20:\\n\",\"color\":\"gray\"}," +
-            "{\"text\":\"  x3 odul\\n\",\"color\":\"green\"}," +
-            "{\"text\":\"Seviye 21-50:\\n\",\"color\":\"gray\"}," +
-            "{\"text\":\"  x4 odul\\n\",\"color\":\"green\"}," +
-            "{\"text\":\"Seviye 50+:\\n\",\"color\":\"gray\"}," +
-            "{\"text\":\"  x5 odul\",\"color\":\"gold\",\"bold\":true}" +
-            "]}", null)));
-
-        pages.add(RawFilteredPair.of(Text.Serialization.fromJsonString(
-            "{\"text\":\"\",\"extra\":[" +
-            "{\"text\":\"Sans Yumurtasi\\n\",\"color\":\"gold\",\"bold\":true}," +
-            "{\"text\":\"\\nCraft Tarifi:\\n\",\"color\":\"green\",\"bold\":true}," +
-            "{\"text\":\"N  E  N\\n\",\"color\":\"gray\"}," +
-            "{\"text\":\"E  E  E\\n\",\"color\":\"gray\"}," +
-            "{\"text\":\"N  E  N\\n\",\"color\":\"gray\"}," +
-            "{\"text\":\"\\nN=Netherite\\n\",\"color\":\"dark_purple\"}," +
-            "{\"text\":\"E=Elmas\\n\",\"color\":\"aqua\"}," +
-            "{\"text\":\"\\nKoy - Sans Ustasi!\",\"color\":\"yellow\"}" +
-            "]}", null)));
-
-        pages.add(RawFilteredPair.of(Text.Serialization.fromJsonString(
-            "{\"text\":\"\",\"extra\":[" +
-            "{\"text\":\"Mob Odulleri\\n\",\"color\":\"red\",\"bold\":true}," +
-            "{\"text\":\"\\nZombi: \",\"color\":\"green\"}," +
-            "{\"text\":\"Elmas, Altin Blok\\n\",\"color\":\"gray\"}," +
-            "{\"text\":\"Iskelet: \",\"color\":\"green\"}," +
-            "{\"text\":\"Buyulu Yay\\n\",\"color\":\"gray\"}," +
-            "{\"text\":\"Creeper: \",\"color\":\"green\"}," +
-            "{\"text\":\"Elmas Blok\\n\",\"color\":\"gray\"}," +
-            "{\"text\":\"Orumcek: \",\"color\":\"green\"}," +
-            "{\"text\":\"Zumrut\\n\",\"color\":\"gray\"}," +
-            "{\"text\":\"Enderman: \",\"color\":\"green\"}," +
-            "{\"text\":\"Netherite\",\"color\":\"gray\"}" +
-            "]}", null)));
-
-        pages.add(RawFilteredPair.of(Text.Serialization.fromJsonString(
-            "{\"text\":\"\",\"extra\":[" +
-            "{\"text\":\"Boss Odulleri\\n\",\"color\":\"red\",\"bold\":true}," +
-            "{\"text\":\"\\nWarden:\\n\",\"color\":\"dark_purple\",\"bold\":true}," +
-            "{\"text\":\"Netherite Set, Elytra\\n\",\"color\":\"gray\"}," +
-            "{\"text\":\"\\nWither:\\n\",\"color\":\"dark_purple\",\"bold\":true}," +
-            "{\"text\":\"Elytra, Nether Star\\n\",\"color\":\"gray\"}," +
-            "{\"text\":\"\\nEnder Dragon:\\n\",\"color\":\"dark_purple\",\"bold\":true}," +
-            "{\"text\":\"TAM Netherite Set + Nether Star x3\",\"color\":\"gray\"}" +
-            "]}", null)));
-
-        pages.add(RawFilteredPair.of(Text.Serialization.fromJsonString(
-            "{\"text\":\"\",\"extra\":[" +
-            "{\"text\":\"\\n\\n\\nBol Sans!\\n\",\"color\":\"gold\",\"bold\":true}," +
-            "{\"text\":\"\\n- Sans Ustasi -\",\"color\":\"gray\"}" +
-            "]}", null)));
+        List<RawFilteredPair<Text>> pages = List.of(
+            RawFilteredPair.of(Text.literal(
+                "Lucky Ascension\n\nHos Geldin!\n\nDusman yaratiklar oldurunce OP esyalar kazan ve sans seviyeni yukseltin!"
+            )),
+            RawFilteredPair.of(Text.literal(
+                "Sans Sistemi\n\nSeviye 1-5: x1 odul\nSeviye 6-10: x2 odul\nSeviye 11-20: x3 odul\nSeviye 21-50: x4 odul\nSeviye 50+: x5 odul"
+            )),
+            RawFilteredPair.of(Text.literal(
+                "Sans Yumurtasi\n\nCraft Tarifi:\nN E N\nE E E\nN E N\n\nN=Netherite\nE=Elmas\n\nKoy - Sans Ustasi!"
+            )),
+            RawFilteredPair.of(Text.literal(
+                "Mob Odulleri\n\nZombi: Elmas, Altin Blok\nIskelet: Buyulu Yay\nCreeper: Elmas Blok\nOrumcek: Zumrut\nEnderman: Netherite"
+            )),
+            RawFilteredPair.of(Text.literal(
+                "Boss Odulleri\n\nWarden:\nNetherite Set, Elytra\n\nWither:\nElytra, Nether Star\n\nEnder Dragon:\nTAM Netherite Set + Nether Star x3"
+            )),
+            RawFilteredPair.of(Text.literal(
+                "\n\n\nBol Sans!\n\n- Sans Ustasi -"
+            ))
+        );
 
         WrittenBookContentComponent content = new WrittenBookContentComponent(
             RawFilteredPair.of("Lucky Ascension Rehberi"),
